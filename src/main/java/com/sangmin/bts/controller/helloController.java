3e8d6b7c823ffb9b80a22beec2ec05cd16276651
Sangmin Lee <tkdals2317@naver.com>
2023-04-30 23:05:42 +0900

    :sparkles: API 테스트 및 테스트 확인을 위한 DB 관련 종속성 제거

--- src/main/java/com/sangmin/bts/controller/helloController.java
+++ src/main/java/com/sangmin/bts/controller/helloController.java
@@ -1,2 +1,13 @@
-package com.sangmin.bts.controller;public class helloController {
+package com.sangmin.bts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "BTS 프로젝트에 오신걸 환영합니다";
+    }
 }
